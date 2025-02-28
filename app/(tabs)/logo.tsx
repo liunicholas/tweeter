a2a2349c1a18241410a2099cb37a8e9345ec4468
Eddie Qiao <eddieqiao@gmail.com>
2025-02-28 03:18:35 -0500

    functionality for double tapping image

--- app/(tabs)/logo.tsx
+++ app/(tabs)/logo.tsx
@@ -1,15 +1,42 @@
 import React, { useState, useEffect, useCallback, useRef } from "react";
 import { View, StyleSheet, Image } from "react-native";
-import Animated from "react-native-reanimated";
+import { Gesture, GestureDetector } from "react-native-gesture-handler";
+import Animated, {
+  useAnimatedStyle,
+  useSharedValue,
+  withSpring,
+} from "react-native-reanimated";
+
+const imageSize = 100;
 
 export default function LogoScreen() {
+  const scaleImage = useSharedValue(imageSize);
+  const doubleTap = Gesture.Tap()
+    .numberOfTaps(2)
+    .onStart(() => {
+      if (scaleImage.value !== imageSize * 2) {
+        scaleImage.value = scaleImage.value * 2;
+      } else {
+        scaleImage.value = Math.round(scaleImage.value / 2);
+      }
+    });
+
+  const imageStyle = useAnimatedStyle(() => {
+    return {
+      width: withSpring(scaleImage.value),
+      height: withSpring(scaleImage.value),
+    };
+  });
+
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Animated.Image
-        source={require("../../assets/images/logo.png")}
-        resizeMode="contain"
-        style={{ width: 100, height: 100 }}
-      />
+      <GestureDetector gesture={doubleTap}>
+        <Animated.Image
+          source={require("../../assets/images/logo.png")}
+          resizeMode="contain"
+          style={[imageStyle, { width: imageSize, height: imageSize }]}
+        />
+      </GestureDetector>
     </View>
   );
 }
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,10 +1,13 @@
 import { Stack } from 'expo-router';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
 export default function RootLayout() {
   return (
-    <Stack>
-      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-      <Stack.Screen name="+not-found" />
-    </Stack>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <Stack>
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen name="+not-found" />
+      </Stack>
+    </GestureHandlerRootView>
   );
 }
